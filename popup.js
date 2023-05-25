110ccf90fc4435a5b4034b3862160ea73d4dad94
Abhinaw Tiwari <abhinawtiwari@Abhinaws-MacBook-Pro.local>
2023-05-26 00:34:41 +0100

    UI cleaning

--- popup.js
+++ popup.js
@@ -4,7 +4,23 @@ document.addEventListener('DOMContentLoaded', function() {
   
     // Render tasks on the popup
     renderTasks();
+    // Attach the keydown event handler to the input element
+    const inputBox = document.getElementById('taskInput');
+    inputBox.addEventListener('keydown', function(event) {
+      // Check if the pressed key is Enter (keycode 13)
+      if (event.keyCode === 13) {
+        // Perform the desired action or call a function
+        const task = inputBox.value.trim();
   
+        if (task !== '') {
+          tasks.push(task);
+          localStorage.setItem('tasks', JSON.stringify(tasks));
+          inputBox.value = '';
+          renderTasks();
+        }
+      }
+    });
+
     // Handle add task button click
     document.getElementById('addButton').addEventListener('click', function() {
       const input = document.getElementById('taskInput');
@@ -20,8 +36,12 @@ document.addEventListener('DOMContentLoaded', function() {
   
     // Handle delete task button click
     document.getElementById('taskList').addEventListener('click', function(event) {
-      if (event.target.classList.contains('deleteButton')) {
-        const index = event.target.dataset.index;
+      // console.log(event.target.classList);
+      const parentElement = event.target.parentElement;
+      // console.log('parentElement: ', event.target.parentElement);
+      // console.log('parent classname: ', parentElement.className);
+      if (parentElement.className === 'deleteIcon') {
+        const index = parentElement.dataset.index;
         tasks.splice(index, 1);
         localStorage.setItem('tasks', JSON.stringify(tasks));
         renderTasks();
@@ -37,9 +57,10 @@ document.addEventListener('DOMContentLoaded', function() {
         const li = document.createElement('li');
         li.textContent = task;
         
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Delete';
-        deleteButton.className = 'deleteButton';
+        const deleteButton = document.createElement('span');
+        // deleteButton.textContent = 'Delete';
+        deleteButton.innerHTML = '<i class="fas fa-times"></i>'; // Add Font Awesome cross icon
+        deleteButton.className = 'deleteIcon';
         deleteButton.dataset.index = index;
         
         li.appendChild(deleteButton);
